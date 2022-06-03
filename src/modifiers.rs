434c99ce309c507ff568618d2d591f3609fb4c71
grantyuan <ys-david@163.com>
2022-06-04 00:54:32 +0800

    added modifier

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -115,6 +115,76 @@ impl InitModifier for PositionCircleModifier {
     }
 }
 
+#[derive(Clone, Copy)]
+pub struct PositionModifier {
+    /// The center, relative to the emitter position.
+    pub center: Vec3,
+    /// The circle axis, which is the normalized normal of the circle's plane.
+    /// Set this to `Vec3::Z` for a 2D game.
+    pub axis: Vec3,
+    /// The circle radius.
+    pub radius: f32,
+    /// The shape dimension to spawn from.
+    pub dimension: ShapeDimension,
+
+
+}
+
+impl Default for PositionModifier {
+    fn default() -> Self {
+        Self {
+            center: Default::default(),
+            axis: Vec3::Z,
+            radius: Default::default(),          
+            dimension: Default::default(),
+        }
+    }
+}
+
+impl InitModifier for PositionModifier {
+    fn apply(&self, init_layout: &mut InitLayout) {
+        let (tangent, bitangent) = self.axis.any_orthonormal_pair();
+
+        let radius_code = match self.dimension {
+            ShapeDimension::Surface => {
+                // Constant radius
+                format!("let r = {};", self.radius.to_wgsl_string())
+            }
+            ShapeDimension::Volume => {
+                // Radius uniformly distributed in [0:1], then square-rooted
+                // to account for the increased perimeter covered by increased radii.
+                format!("let r = sqrt(rand()) * {};", self.radius.to_wgsl_string())
+            }
+        };
+
+        init_layout.position_code = format!(
+            r##"
+    // >>> [PositionCircleModifier]
+    // Circle center
+    let c = {};
+    // Circle basis
+    let tangent = {};
+    let bitangent = {};
+    // Circle radius
+    {}
+
+    // Spawn random point on/in circle
+    let theta = rand() * tau;
+    let dir = tangent * cos(theta) + bitangent * sin(theta);
+    ret.pos = c + r * dir;
+    // Velocity away from center
+    ret.vel = dir * speed;
+    // <<< [PositionCircleModifier]
+            "##,
+            self.center.to_wgsl_string(),
+            tangent.to_wgsl_string(),
+            bitangent.to_wgsl_string(),
+            radius_code,
+        );
+    }
+}
+
+
 /// An initialization modifier spawning particles on a sphere.
 #[derive(Default, Clone, Copy)]
 pub struct PositionSphereModifier {
